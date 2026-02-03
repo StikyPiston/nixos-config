3232e58ca00494b4d03e40fdefc76b7fd51fae57
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-02-03 17:20:26 +0200

    Fix nvf config

--- configs/nvf.nix
+++ configs/nvf.nix
@@ -6,36 +6,28 @@
 }:
 
 {
+  vim = {
 
-  programs.nvf = {
-    enable = true;
-    settings = {
+    theme.enable = true;
+    theme.name = "catppuccin";
+    theme.style = "mocha";
 
-      vim = {
-
-        theme.enable = true;
-        theme.name = "catppuccin";
-        theme.style = "mocha";
-
-        languages = {
-          enableLSP = true;
-          enableTreesitter = true;
-          lua.enable = true;
-          ruby.enable = true;
-          go.enable = true;
-          markdown.enable = true;
-          nix.enable = true;
-          typst.enable = true;
-        };
-
-        statusline.lualine.enable = true;
-        telescope.enable = true;
-        autocomplete.ncmp.enable = true;
-        lazy.enable = true;
-
-      };
-
+    languages = {
+      enableLSP = true;
+      enableTreesitter = true;
+      lua.enable = true;
+      ruby.enable = true;
+      go.enable = true;
+      markdown.enable = true;
+      nix.enable = true;
+      typst.enable = true;
     };
+
+    statusline.lualine.enable = true;
+    telescope.enable = true;
+    autocomplete.ncmp.enable = true;
+    lazy.enable = true;
+
   };
 
 }
