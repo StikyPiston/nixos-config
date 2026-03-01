cd8ed65adbe4598bd83a7a07ac8f7c6d718ee796
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-03-01 09:30:54 +0200

    Switch from sudo to doas

--- configs/security.nix
+++ configs/security.nix
@@ -1,11 +1,28 @@
 {
+  pkgs,
   ...
 }:
 
 {
 
-  security.sudo.extraConfig = ''
-    Defaults passprompt=" Password: "
-  '';
+  # security.sudo.extraConfig = ''
+  #   Defaults passprompt=" Password: "
+  # '';
+
+  security.sudo.enable = false;
+  security.doas = {
+    enable = true;
+    extraRules = [
+      {
+        groups = [ "wheel" ];
+        persist = true;
+        keepEnv = true;
+      }
+    ];
+  };
+
+  environment.systemPackages = [
+    (pkgs.writeScriptBin "sudo" ''exec doas "$@"'')
+  ];
 
 }
