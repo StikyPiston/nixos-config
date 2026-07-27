d6e57d061c2c01cb1e1d03d8490539a2a786d2b7
indium114 <156162907+indium114@users.noreply.github.com>
2026-07-27 19:24:13 +0200

    nixos(disk.nix, TODO.recall): disko config with luks
    just the basic config for now, has yet to be incorporated into the wider
    configuration

---
+++ disk.nix
@@ -0,0 +1,36 @@
+{
+  disko.devices = {
+    disk.main = {
+      type = "disk";
+      device = "/dev/sda";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size = "500M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+          luks = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "nixos";
+              settings.allowDiscards = false;
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
