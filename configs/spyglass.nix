f7934f523792a2879189b46320dd024067c7f9bb
indium114 <156162907+indium114@users.noreply.github.com>
2026-08-02 11:59:11 +0200

    home-manager(configs/spyglass.nix): configure spyglass web lens

--- configs/spyglass.nix
+++ configs/spyglass.nix
@@ -5,5 +5,8 @@
 {
 
   xdg.configFile."spyglass/applications".source = ./spyglass/applications;
+  xdg.configFile."spyglass/web/config.toml".text = ''
+    url = "http://127.0.0.1:5000/search?q=%s"
+  '';
 
 }
